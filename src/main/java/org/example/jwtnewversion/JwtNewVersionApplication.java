7ca405a05c47776d57a293cfcbc8fe685a3f8624
Ali Nacafov <alinacaf10@gmail.com>
2024-01-16 18:08:08 +0400

    creating

---
+++ src/main/java/org/example/jwtnewversion/JwtNewVersionApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtnewversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtNewVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtNewVersionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jwtnewversion/JwtNewVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtnewversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtNewVersionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
