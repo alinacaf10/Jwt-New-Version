6e817b54b1eac35278e5d4cf7cc0b7a78f39c230
Ali Nacafov <alinacaf10@gmail.com>
2024-01-16 19:08:59 +0400

    creating OurUsers, Product.java

--- src/main/java/org/example/jwtnewversion/dto/ReqRes.java
+++ src/main/java/org/example/jwtnewversion/dto/ReqRes.java
@@ -1,4 +1,27 @@
 package org.example.jwtnewversion.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.example.jwtnewversion.entity.Product;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)//Sınıfın tüm alanları görmezden gelinir ve JSON temsilinde yer almaz.
+@JsonInclude(JsonInclude.Include.NON_NULL)//Null değere sahip olan alanlar JSON çıktısında görünmez.
 public class ReqRes {
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private List<Product> products;
+    private String ourUsers;
+
 }
---
+++ src/main/java/org/example/jwtnewversion/entity/Product.java
@@ -0,0 +1,14 @@
+package org.example.jwtnewversion.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
