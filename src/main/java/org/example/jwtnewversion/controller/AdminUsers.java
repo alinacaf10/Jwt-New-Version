94435f4c5459ef5aaacac8190b95d52ed6bd846f
Ali Nacafov <alinacaf10@gmail.com>
2024-01-17 21:18:22 +0400

    some addition left for AuthFilter

---
+++ src/main/java/org/example/jwtnewversion/controller/AdminUsers.java
@@ -0,0 +1,42 @@
+package org.example.jwtnewversion.controller;
+
+
+import org.example.jwtnewversion.dto.ReqRes;
+import org.example.jwtnewversion.entity.Product;
+import org.example.jwtnewversion.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminUsers {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @GetMapping("/public/product")
+    public ResponseEntity<Object> getAllProducts(){
+        return ResponseEntity.ok(productRepo.findAll());
+    }
+
+    @PostMapping("/admin/saveproduct")
+    public ResponseEntity<Object> signUp(@RequestBody ReqRes productRequest){
+        Product productToSave = new Product();
+        productToSave.setName(productRequest.getName());
+        return ResponseEntity.ok(productRepo.save(productToSave));
+    }
+
+
+    @GetMapping("/user/alone")
+    public ResponseEntity<Object> userAlone(){
+        return ResponseEntity.ok("USers alone can access this ApI only");
+    }
+
+    @GetMapping("/adminuser/both")
+    public ResponseEntity<Object> bothAdminaAndUsersApi(){
+        return ResponseEntity.ok("Both Admin and Users Can  access the api");
+    }
+}
---
+++ src/main/java/org/example/jwtnewversion/controller/AuthController.java
@@ -0,0 +1,28 @@
+package org.example.jwtnewversion.controller;
+
+
+import org.example.jwtnewversion.dto.ReqRes;
+import org.example.jwtnewversion.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ReqRes> signUp(@RequestBody ReqRes signUpRequest){
+        return ResponseEntity.ok(authService.signUp(signUpRequest));
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<ReqRes> signIn(@RequestBody ReqRes signInRequest){
+        return ResponseEntity.ok(authService.signIn(signInRequest));
+    }
+    }
